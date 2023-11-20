630282775d4976c87f4c9f41b0c78ce81f2018be
Wellington Mariano Nunes <wellingtonnunez10@gmail.com>
2023-11-20 09:00:12 -0300

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { SafeAreaView, Text } from 'react-native';
+import Login from './modules/login';
 
 function App() {
   return (
     <SafeAreaView>
-      <Text>Hello world</Text>
+      <Login/>
     </SafeAreaView>
   );
 }
